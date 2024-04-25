e58a26702ce9c12caa42b619d0b218427e4bbb72
Vitalii Shapovalov <martyit@ukr.net>
2024-04-25 21:52:47 +0200

    - Made reuse button, depends what name of the button you will choose, it will change the size.
    - added folder style, where I want to store all styles files

--- src/Components/root.jsx
+++ src/Components/root.jsx
@@ -1,24 +1,15 @@
 import logo from "../assets/images/FirstPageAlienLOGO.svg"
-import '../assets/images.css'
+import MainButton  from "./mainButton/mainButton"
+
 export default function Root() {
-
-
-
+	const nameBtn = 'Play';
 	return (
 		<>
 			<div className="container sky-background">
 				<div className="content">
 					<img className="logo" src={logo} alt="Logo" />
 				</div>
-				<div className="btn__box">
-					<div className="btn__layer-1">
-						<div className="btn__layer-2">
-							<div className="btn__layer-3">
-								<button type='button' className='btn'>Start</button>
-							</div>
-						</div>
-					</div>
-				</div>
+				<MainButton name={nameBtn} />
 			</div>
 		</>
 	)
