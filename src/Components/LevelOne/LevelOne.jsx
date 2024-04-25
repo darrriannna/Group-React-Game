2d9a1a5fcb327f55fe1edf07296bf6457ada3bea
Esmatt123 <105745382+Esmatt123@users.noreply.github.com>
2024-04-25 16:56:59 +0200

    Finished the bare layout of level one, added montserrat font, heart icon, pause icon and sound icon to the level

--- src/Components/LevelOne/LevelOne.jsx
+++ src/Components/LevelOne/LevelOne.jsx
@@ -1,11 +1,34 @@
 import styles from './LevelOne.module.css'
+import pauseButton from '../../assets/icons/Pause-icon.svg'
+import heart from '../../assets/icons/Heart-game-red.svg'
+import soundOn from '../../assets/icons/Sound-ON-icon.svg'
+
+
 
 const LevelOne = () => {
     return (
         <>
-        <div className={styles.levelContainer}>
-        hello
-        </div>
+            <div className={styles.levelContainer}>
+                <div className={styles.topContainer}>
+                <div className={styles.miniContainer1}>
+                <div className={styles.svg}>
+                <img src={pauseButton} alt="Pause" />
+                </div>
+                <p className={styles.gameText}>Player 1</p>
+                </div>
+                <div className={styles.miniContainer2}>
+                <div className='hearts'>
+                    <img src={heart} alt="Heart" />
+                    <img src={heart} alt="Heart" />
+                    <img src={heart} alt="Heart" />
+                </div>
+                <p className={styles.gameText}>Score: 0</p>
+                </div>
+                </div>
+                <div className={styles.soundIcon}>
+                    <img src={soundOn}/>
+                </div>
+            </div>
         </>
     )
 }
