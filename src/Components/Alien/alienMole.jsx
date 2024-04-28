a9f4ec4a4cb9b10c9a4e109f5b7a7785cd390d53
Vitalii Shapovalov <martyit@ukr.net>
2024-04-28 23:04:29 +0200

    Scores are working and fixed bugs with timers and appears. Fixed timer conflict after clicking on alien. added  one check if alien is hidden.then i can run a timer to hide them. Before just new timer running and old timer still working and this made a timer conflict.
    - Made visual effect of clicking. Next step to add hammer and animation effect

--- src/Components/Alien/alienMole.jsx
+++ src/Components/Alien/alienMole.jsx
@@ -9,48 +9,70 @@ import { useState, useEffect } from 'react';
 // eslint-disable-next-line react/prop-types
 const AlienMole = ({ alienId, onAlienClick }) => {
 
-  const [isHidden, setIsHidden] = useState(false);
-
-  const [isClicked, setIsClicked] = useState(false);  
+  const [isHidden, setIsHidden] = useState(true);
+  const [isClicked, setIsClicked] = useState(false);  //Alien hidden in the beginning
+  const [appearTimer, setAppearTimer] = useState(null)
+  const [disappearTimer, setDisappearTimer] = useState(null)
   
   const handleClick = function () {
     setIsClicked(true)
-    setIsHidden(true)
+	  setIsHidden(false)
+	clearTimeout(appearTimer)
+	clearTimeout(disappearTimer)
 	 
     setTimeout(() => {
-		 setIsHidden(false)
+		 setIsHidden(true)
 		 if (!isClicked) {
 			 scheduleNextAppearance();
+			 setTimeout(() => {
+				 setIsClicked(false)
+			 }, 300);
+			} else{
+				setTimeout(() => {
+					setIsClicked(false)
+				}, 300);
+				// scheduleNextAppearance();
 			}
 		}, 1000);
 		onAlienClick()
   }
 
-  useEffect(() => {
-
+  useEffect(() => {	
+//start timers after creating components
     scheduleNextAppearance()
 
-    return () => clearTimeout();
+
+    return () => clearTimeout(appearTimer, disappearTimer);
   }, []);
 
 
   const scheduleNextAppearance = () => {
+	
+	//first timer for make them visible
+	if(isHidden) {
 
-    setTimeout(() => {
-      setIsHidden(true);
-      setTimeout(() => {
-          setIsHidden(false);
-          scheduleNextAppearance();
-      }, getRandomDelay(5000, 3000)); // Between 3000ms and 5000ms
-  }, getRandomDelay(8000, 3000)); // Between 5000ms and 8000ms
-  };
+		const appearTimer = setTimeout(() => {	
+			//Aliens are visible   
+			setIsHidden(false);
+			
+			//second timer to make them hidden
+			const disappearTimer = setTimeout(() => {
+					setIsHidden(true);
+					
+					scheduleNextAppearance();
+				}, getRandomDelay(5000, 3000)); // Between 3000ms and 5000ms
+				setAppearTimer(appearTimer)
+		}, getRandomDelay(8000, 3000)); // Between 5000ms and 8000ms
+		setDisappearTimer(disappearTimer)
+		};
+	}
 
   const getRandomDelay = (max, min) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   return (
-    <img className={`${styles.alien} ${alienId} ${isHidden ? styles.disappear : styles.appear}`} src={alien} alt='alien' onClick={handleClick} />
+    <img className={`${styles.alien} ${alienId} ${isHidden ? styles.disappear : styles.appear} ${isClicked ? styles.punched : ''}`} src={alien} alt='alien' onClick={handleClick} />
   )
 }
 
