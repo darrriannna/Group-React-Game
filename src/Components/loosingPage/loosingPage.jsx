6388fb18a0efcb3cda65c02781657b3d22487268
KwabenaAnane <157129714+KwabenaAnane@users.noreply.github.com>
2024-05-05 16:21:13 +0200

    Adding info the loosing Page
    Adding flying img, score, go back menu and more

--- src/Components/loosingPage/loosingPage.jsx
+++ src/Components/loosingPage/loosingPage.jsx
@@ -1,6 +1,31 @@
+// import React from "react";
 import styles from "./loosingPage.module.css";
+import goBackMenu from "../../assets/icons/Go-back-menu.svg";
+import astronautFlying from "../../assets/images/austronaut-levitating.svg";
+import { useState } from "react";
 
 const LoosingPage = () => {
-  return <p className={styles.youLost}>Hello World!</p>;
+  return (
+    <div className={styles.loosingPageContainer}>
+      <div className={styles.topDiv}>
+        <div className={styles.goBack}>
+          <img src={goBackMenu} alt="Go back icon" />
+        </div>
+
+        <div className={styles.loosingInfo}>
+          <h1>You lost all lives!</h1>
+          <h2>Now aliens are living on the moon</h2>
+        </div>
+      </div>
+
+      <div className={styles.middleDiv}>
+        <div className={styles.flyingAstronaut}>
+          <img src={astronautFlying} alt="flying astronaut" />
+        </div>
+
+        <p className={styles.loosingPageScore}>Your Score: {score}</p>
+      </div>
+    </div>
+  );
 };
 export default LoosingPage;
