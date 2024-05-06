e3f56327b90577db8af28f542ce2d7486ab0c6c8
darynafesyk <daryna.fesyk@stud.sundsgarden.se>
2024-05-06 16:43:32 +0200

    Adding explosion effect to bombs

--- src/Components/LevelOne/LevelOne.jsx
+++ src/Components/LevelOne/LevelOne.jsx
@@ -8,30 +8,33 @@ import Hammer from '../hammer/hammer';
 import clickPause from '../../../public/music/buttonclick.wav'; 
 import explosionSound from '../../../public/music/bomb-explosion.mp3'
 import GameOverPage from '../gameOver/gameOverPage';
+import ExplosionSVG from '../../assets/images/explosion-boom.svg'; // Import the explosion SVG
 
 const LevelOne = () => {
-	const [score, setScore] = useState(0);
-	const [isPlaying, setIsPlaying] = useState(false);
-	const [hearts, setHearts] = useState(3)
-	const [gameOver, setGameOver] = useState(false)
-	
-	const scoreIncrease = () => {
-		setScore(score + 10);
-	};
-	
-	const bombClick = () =>{
-		audioExplosion.play()
-		if (hearts > 0) {
-			setHearts(hearts -1)
-			// need to add more effects of explosion
+    const [score, setScore] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [hearts, setHearts] = useState(3);
+    const [gameOver, setGameOver] = useState(false);
+    const [showExplosion, setShowExplosion] = useState(false); // State to control the visibility of the explosion SVG
 
-		}
-	}
+    const scoreIncrease = () => {
+        setScore(score + 10);
+    };
+    
+    const bombClick = () =>{
+        audioExplosion.play();
+        if (hearts > 0) {
+            setHearts(hearts -1);
+            setShowExplosion(true); // Show explosion SVG
+            setTimeout(() => {
+                setShowExplosion(false); // Hide explosion SVG after 3 seconds
+            }, 1000);
+        }
+    };
 
+    const audioClick = new Audio(clickPause);
+    const audioExplosion = new Audio(explosionSound);
 
-	const audioClick = new Audio(clickPause);
-	const audioExplosion = new Audio(explosionSound)
-	
     const handleClick = () => {
         if (!isPlaying) {
             audioClick.play();
@@ -40,10 +43,9 @@ const LevelOne = () => {
                 setIsPlaying(false);
             });
         }
-
     };
 
-	useEffect(() =>{
+  useEffect(() =>{
 		if (hearts === 0) {
 			setGameOver(true)
 		}
@@ -51,43 +53,43 @@ const LevelOne = () => {
 
     return (
         <>
-		  {
-			gameOver ? (
-				<GameOverPage score = {score}/>
-			) :(
-            <div className={`${styles.levelContainer}`}>
-                <div className={styles.topContainer}>
-                    <div className={styles.miniContainer1}>
-                        <div className={styles.svg}>
-                            <img src={pauseButton} alt="Pause" onClick={handleClick} />
+            {gameOver ? (
+                <GameOverPage score = {score}/>
+            ) : (
+                <div className={`${styles.levelContainer}`}>
+                    <div className={styles.topContainer}>
+                        <div className={styles.miniContainer1}>
+                            <div className={styles.svg}>
+                                <img src={pauseButton} alt="Pause" onClick={handleClick} />
+                            </div>
+                            <p className={styles.gameText}>Player 1</p>
                         </div>
-                        <p className={styles.gameText}>Player 1</p>
-                    </div>
-                    <div className={styles.miniContainer2}>
-                        <div className='hearts'>
-									{Array.from({length: 3}, (_, index) =>(
-										<img 
-										key={index} 
-										src={index < hearts ? heart : grayHeart} 
-										alt= 'Heart'/>
-									))}
+                        <div className={styles.miniContainer2}>
+                            <div className='hearts'>
+                                {Array.from({length: 3}, (_, index) =>(
+                                    <img 
+                                    key={index} 
+                                    src={index < hearts ? heart : grayHeart} 
+                                    alt= 'Heart'/>
+                                ))}
+                            </div>
+                            <p className={styles.gameText}>Score: {score}</p>
                         </div>
-                        <p className={styles.gameText}>Score: {score}</p>
                     </div>
+                    <AlienMole  alienId={styles.a1} onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
+                    {showExplosion && <img src={ExplosionSVG} className={`${styles.explosion} ${showExplosion && styles.active}`}/>}
+                    <AlienMole alienId={styles.a2}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
+                    <AlienMole alienId={styles.a3}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
+                    <AlienMole alienId={styles.a4}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
+                    <AlienMole alienId={styles.a5}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
+                    <AlienMole alienId={styles.a6}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
+                    <AlienMole alienId={styles.a7}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
                 </div>
-                <AlienMole  alienId={styles.a1} onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
-                <AlienMole alienId={styles.a2}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
-                <AlienMole alienId={styles.a3}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
-                <AlienMole alienId={styles.a4}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
-                <AlienMole alienId={styles.a5}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
-                <AlienMole alienId={styles.a6}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
-                <AlienMole alienId={styles.a7}  onAlienClick ={scoreIncrease} onBombClick={bombClick}/>
-            </div>
-			)
-		  }
+            )}
             <Hammer />
         </>
     );
 }
 
 export default LevelOne;
+
