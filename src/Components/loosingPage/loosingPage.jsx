656469a0c42af7b023a5a89b2c4252c5f2f71563
KwabenaAnane <157129714+KwabenaAnane@users.noreply.github.com>
2024-05-09 13:48:04 +0200

    Adding crowd-cheer effect to the winPage

--- src/Components/loosingPage/loosingPage.jsx
+++ src/Components/loosingPage/loosingPage.jsx
@@ -1,85 +1,3 @@
-// import { useEffect, useState } from "react";
-
-// import styles from "./loosingPage.module.css";
-// import goBackMenu from "../../assets/icons/Go-back-menu.svg";
-// import astronautFlying from "../../assets/images/austronaut-levitating.svg";
-// import strongAlien from "../../assets/images/Strong-alien-moon.svg";
-
-// // const LoosingPage = () => {
-
-// //   return (
-// //     <div className={styles.loosingPageContainer}>
-// //       <div className={styles.topDiv}>
-// //         <div className={styles.goBack}>
-// //           <img src={goBackMenu} alt="Go back icon" />
-// //         </div>
-
-// //         <div className={styles.loosingInfo}>
-// //           <h1>You lost all lives!</h1>
-// //           <h2>Now aliens are living on the moon</h2>
-// //         </div>
-// //       </div>
-
-// //       <div className={styles.middleDiv}>
-// //         <p className={styles.loosingPageScore}>Your Score: 300</p>
-// //         {/* {score}  have to fix to see real score*/}
-
-// //         <div className={styles.flyingAstronaut}>
-// //           <img src={astronautFlying} alt="flying astronaut" />
-// //         </div>
-// //       </div>
-
-// //       <div className={styles.strongAlien}>
-// //         <img src={strongAlien} alt="Picture of Strong alien on moon" />
-// //       </div>
-// //     </div>
-// //   );
-// // };
-// // export default LoosingPage;
-
-// //New line to modify
-
-// // eslint-disable-next-line react/prop-types
-
-// // eslint-disable-next-line react/prop-types
-// export default function LoosingPage({ score }) {
-//   const [confirmation, setConfirmation] = useState(false);
-
-//   useEffect(() => {
-//     if (!confirmation) {
-//       window.confirm("GAME OVER!");
-//     }
-//     setConfirmation(true);
-//   }, [confirmation]);
-
-//   return (
-//     <div className={styles.loosingPageContainer}>
-//       <div className={styles.topDiv}>
-//         <div className={styles.goBack}>
-//           <img src={goBackMenu} alt="Go back icon" />
-//         </div>
-
-//         <div className={styles.loosingInfo}>
-//           <h1>GAME OVER!!!</h1>
-//           <h2>You lost all your lives!!!</h2>
-//         </div>
-//       </div>
-
-//       <div className={styles.middleDiv}>
-//         <p className={styles.loosingPageScore}>Your Score:{score}</p>
-
-//         <div className={styles.flyingAstronaut}>
-//           <img src={astronautFlying} alt="flying astronaut" />
-//         </div>
-//       </div>
-
-//       <div className={styles.strongAlien}>
-//         <img src={strongAlien} alt="Picture of Strong alien on moon" />
-//       </div>
-//     </div>
-//   );
-// }
-
 import { useEffect, useState } from "react";
 import styles from "./loosingPage.module.css";
 import goBackMenu from "../../assets/icons/Go-back-menu.svg";
@@ -99,8 +17,8 @@ export default function LoosingPage({ score }) {
     setConfirmation(true);
   }, [confirmation]);
 
+  // Function to handle going back to LevelOne page
   const handleGoBack = () => {
-    // Function to handle going back to LevelOne page
     window.location.href = "/levelOne";
     setShowLevelOne(true);
   };
@@ -124,7 +42,7 @@ export default function LoosingPage({ score }) {
           </div>
 
           <div className={styles.middleDiv}>
-            <p className={styles.loosingPageScore}>Your Score:{score}</p>
+            <p className={styles.loosingPageScore}>Your Score: {score}</p>
 
             <div className={styles.flyingAstronaut}>
               <img src={astronautFlying} alt="flying astronaut" />
